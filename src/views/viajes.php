be2a68a591b580da358265ffee6f3bb25c58eba1
csarj <csarjdeveloper@gmail.com>
2023-12-14 16:47:31 -0600

    Commit ejemplo

--- src/views/viajes.php
+++ src/views/viajes.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../public/css/form.css">
     <link rel="stylesheet" href="../../public/css/table.css">
+    
     <title>Registrar Viajes</title>
 </head>
 
